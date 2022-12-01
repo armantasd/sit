f619429a83d2ce68a1ae553f54bda9a2175e854a
armantasd <85786043+armantasd@users.noreply.github.com>
2022-12-01 17:55:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const header = document.querySelector("header")
+const tabs = document.querySelectorAll(".tab")
+
+function showTab(id){
+    header.style.display = "none"
+    for(let x of tabs){
+        x.style.display = "none"
+        x.style.opacity = 0
+    }
+
+    var selectedTab = document.getElementById(id)
+    selectedTab.style.display = 'block'
+
+    //Animacija 
+    setTimeout(function(){
+        selectedTab.style.transition = "opacity 1s"
+        selectedTab.style.opacity = 1
+    })
+}
+function closeTab(event){
+    event.currentTarget.parentNode.style.display = "none"
+    header.style.display = "block"
+}
+
+
+
+
+const intro = document.querySelector(".intro")
+setTimeout(function(){
+    intro.style.height = "200px"
+}, 100)
+
+setTimeout(function(){
+    intro.style.opacity = 1
+}, 400)
